a63fa929208f8da164508b23216378f498e8b0bb
Blake Dietz <blakedietz@gmail.com>
2023-10-25 23:34:44 -0600

    update export

--- private_dot_config/private_fish/exports/lazygit.fish
+++ private_dot_config/private_fish/exports/lazygit.fish
@@ -1,2 +1,2 @@
 # Redefine the location from ~/Library/Application Support/lazygit/config.yml
-set -gx XDG_CONFIG_HOME "/Users/blakedietz/.config"
+set -Ux XDG_CONFIG_HOME "/Users/blakedietz/.config"
