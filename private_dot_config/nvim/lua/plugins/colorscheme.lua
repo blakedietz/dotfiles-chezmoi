6697ab5cb552106e03d3e0fc29a3ea7dfb5a8079
Blake Dietz <blakedietz@gmail.com>
2024-01-22 11:34:19 -0700

    update to latest plugins

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
     "folke/tokyonight.nvim",
     cond = not vim.g.vscode,
     opts = {
-      style = "moon",
+      style = "night",
       dim_inactive = true,
       on_highlights = function(hl, c)
         -- Make borders slightly more legible VertSplit didn't seem to do anything
